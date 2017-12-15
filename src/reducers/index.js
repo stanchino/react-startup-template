af9b8f81b5c8b4800c88b6300e080445949c46f6
Stanimir Dimitrov <stanchino@gmail.com>
2017-12-15 23:42:02 +0200

    Setup cookie storage for redux-persist and configured the authentication expiration to 10 days

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,18 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist'
 import { reducer as form  } from 'redux-form';
 import { routerReducer } from 'react-router-redux';
-import session from 'redux-persist/lib/storage/session';
+import CookieStorage from 'redux-persist-cookie-storage'
 
 import auth from '../auth/reducers';
 
 const config = {
     key: 'auth',
-    storage: session,
+    storage: new CookieStorage({
+        expiration: {
+            default: 30 * 86400,
+            'persist:auth': 10 * 86400
+        }
+    }),
 };
 
 export default combineReducers({ auth: persistReducer(config, auth), form, routerReducer });
